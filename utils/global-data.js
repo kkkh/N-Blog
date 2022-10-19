221a627dc36acd3b758ac952066d60b9a6456051
秦超 <115026744+kkkh@users.noreply.github.com>
2022-10-19 16:21:00 +0800

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : '超';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : '小蓝色';
